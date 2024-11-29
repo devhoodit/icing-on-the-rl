e38036fd64b54c7b77fc1db4c62ce91facfbb612
devhoodit <hoodit@naver.com>
2024-11-29 15:00:16 +0900

    change: replay memory base class

--- irl/replay_memory/replay_memory.py
+++ irl/replay_memory/replay_memory.py
@@ -4,12 +4,40 @@ from __future__ import annotations
 
 import random
 from collections import deque
-from typing import Generic, TypeVar
+from typing import Generic, TypeVar, MutableSequence, Any
 
 T = TypeVar("T")
 
+class BaseReplayMemory(Generic[T]):
+    def __init__(self, factory: type[MutableSequence], **kwargs):
+        self.factory = factory
+        self.kwargs = kwargs
+        self.memory = self._create_memory(self.factory, self.kwargs)
+        
+    def get_memory(self) -> MutableSequence[T]:
+        return self.memory
 
-class ReplayMemory(Generic[T]):
+    def push(self, transition: T) -> None:
+        "push transition"
+        self.memory.append(transition)
+    
+    def sample(self, batch_size: int) -> list[T]:
+        raise NotImplementedError()
+    
+    def _create_memory(self, factory: type[MutableSequence], kwargs: dict[str, Any]) -> MutableSequence:
+        if not kwargs:
+            return factory()
+        return factory(**kwargs)
+    
+    def clear(self) -> None:
+        self.memory = self._create_memory(self.factory, self.kwargs)
+    
+    def __len__(self) -> int:
+        """memory length"""
+        return len(self.memory)
+
+
+class ReplayMemory(BaseReplayMemory[T]):
     """Base replay memory."""
 
     def __init__(
@@ -23,17 +51,9 @@ class ReplayMemory(Generic[T]):
             capacity (int): replay queue capacity
 
         """
+        super().__init__(deque, {"maxlen": capacity})
         self.capacity = capacity
-        self.queue: deque[T] = deque([], self.capacity)
-
-    def push(self: ReplayMemory, transition: T) -> None:
-        """Push transition into memory."""
-        self.queue.append(transition)
 
     def sample(self: ReplayMemory, batch_size: int) -> list[T]:
         """Sample batch from queue."""
-        return random.sample(self.queue, batch_size)
-
-    def __len__(self: ReplayMemory) -> int:
-        """Return memory size."""
-        return len(self.queue)
+        return random.sample(self.memory, batch_size)
--- irl/replay_memory/rollout_buffer.py
+++ irl/replay_memory/rollout_buffer.py
@@ -2,10 +2,20 @@
 
 from __future__ import annotations
 
-from typing import Generic, TypeVar
+from typing import TypeVar
+from irl.logger import get_module_logger
+from .replay_memory import BaseReplayMemory
 
 T = TypeVar("T")
 
-
-class RolloutBuffer(list[T]):
+class RolloutBuffer(BaseReplayMemory[T]):
     """Base Rollout Buffer."""
+    def __init__(self, is_log=True):
+        super().__init__(list)
+        self.is_log = True
+    
+    def sample(self, batch_size) -> list[T]:
+        """rollout buffer sample always return empty list"""
+        if self.is_log:
+            get_module_logger().warning("rollout buffer sample always return empty list, to turn off this message set RolloutBuffer(is_log=False) or set module log level greater than warning")
+        return []
